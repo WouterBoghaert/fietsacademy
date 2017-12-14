669755111ca6f8a6ca4bcba0757b4d2b44d656fb
Wouter Boghaert <Wouter Boghaert@192.168.0.104>
2017-12-14 17:15:08 +0100

    Hoofdstuk 26

--- src/main/java/be/vdab/entities/Campus.java
+++ src/main/java/be/vdab/entities/Campus.java
@@ -9,11 +9,13 @@ import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
@@ -38,6 +40,9 @@ public class Campus implements Serializable {
 	@OneToMany(mappedBy ="campus")
 	@OrderBy("voornaam, familienaam")
 	private Set<Docent> docenten;
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="managerid")
+	private Manager manager;
 	
 	public Campus(String naam, Adres adres) {
 		setNaam(naam);
@@ -96,6 +101,10 @@ public class Campus implements Serializable {
 		if(docent.getCampus() == this) {
 			docent.setCampus(null);
 		}
+	}
+	
+	public Manager getManager() {
+		return manager;
 	}
 
 	@Override
